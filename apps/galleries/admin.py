fb132ec4fb5d23f797acc1409d62c1e3c205a483
Akzam Khamitov <makoron3002@gmail.com>
2023-05-13 08:57:53 +0600

    Created Picture and Gallery models, registered them in the admin panel

--- apps/galleries/admin.py
+++ apps/galleries/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Gallery, Picture
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'icon')
+    list_filter = ('user',)
+    search_fields = ('name', 'user__username')
+
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gallery', 'created_at', 'image_tag')
+    list_filter = ('gallery',)
+    search_fields = ('name', 'gallery__name')
+
+    def image_tag(self, obj):
+        return format_html('<img class="image" src="{}" />'.format(obj.avatar.url))
+
+    image_tag.short_description = 'Image'
--- apps/galleries/models.py
+++ apps/galleries/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Gallery(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='galleries',
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    icon = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+
+    class Meta:
+        unique_together = ('user', 'name')
+
+    def __str__(self):
+        return self.name
+
+
+class Picture(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    gallery = models.ForeignKey(
+        Gallery,
+        on_delete=models.CASCADE,
+        related_name='pictures',
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        null=False,
+        blank=False,
+    )
+    image = models.ImageField(
+        upload_to='pictures/',
+        null=False,
+        blank=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=False
+    )
+
+    class Meta:
+        unique_together = ('gallery', 'name')
+
+    def __str__(self):
+        return self.name
